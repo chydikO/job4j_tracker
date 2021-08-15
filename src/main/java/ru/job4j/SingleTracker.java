67134e81c0deba6d2a5d83031a5c8cd45b977289
Chudnovskiy.O.W520 <oleg.chudnovskiy@gmail.com>
2021-08-15 12:32:59 +0300

    2. Singleton [#94606]00 refactoring

--- src/main/java/ru/job4j/SingleTracker.java
+++ src/main/java/ru/job4j/SingleTracker.java
@@ -1,6 +1,5 @@
 package ru.job4j;
 
-import ru.job4j.tracker.Item;
 import ru.job4j.tracker.Tracker;
 
 public final class SingleTracker {
@@ -10,27 +9,7 @@ public final class SingleTracker {
 
     }
 
-    public Item add(Item item) {
-        return tracker.add(item);
-    }
-
-    public Item findById(int id) {
-        return tracker.findById(id);
-    }
-
-    public boolean replace(int id, Item item) {
-        return tracker.replace(id, item);
-    }
-
-    public boolean delete(int id) {
-        return tracker.delete(id);
-    }
-
-    public Item[] findAll() {
-        return tracker.findAll();
-    }
-
-    public Item[] findByName(String key) {
-        return tracker.findByName(key);
+    public static Tracker getTracker() {
+        return tracker;
     }
 }
