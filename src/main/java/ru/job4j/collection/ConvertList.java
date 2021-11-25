d16a318e7d00597d5fffc7195785a3c3f425f864
Chudnovskiy.O.W520 <oleg.chudnovskiy@gmail.com>
2021-11-25 19:38:22 +0200

    5. Конвертация листа массивов в один лист Integer [#10037]06

---
+++ src/main/java/ru/job4j/collection/ConvertList.java
@@ -0,0 +1,22 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Вам нужно будет преобразовать список с массивом чисел в список чисел.
+ * Метод convert должен пройтись по всем элементам всех массивов в списке list и добавить их в список rsl.
+ * Массивы в списке list могут быть разного размера.
+ */
+public class ConvertList {
+        public static List<Integer> convert(List<int[]> list) {
+            List<Integer> rsl = new ArrayList<>();
+            /// for-each
+            for (int[] row : list) {
+                for (int col: row) {
+                    rsl.add(col);
+                }
+            }
+            return rsl;
+        }
+}
