e336cd79d7770b520be763861cd39da669ff4f5b
MbPro13_chudikO <oleg.chudnovskiy.gmail.com>
2022-01-12 20:45:31 +0200

    0. Map, HashMap [#212652]010

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,18 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("gucodauzinnau-5540@yopmail.com ", "Кузнецов Даниэль Сергеевич");
+        hashMap.put("veuppeifiddeifrau-9642@yopmail.com ", "Кудрявцев Иван Александрович");
+        hashMap.put("frannazotretra-9119@yopmail.com ", "Зубова Валерия Никитична");
+
+        for (Map.Entry<String, String> entry: hashMap.entrySet() ) {
+            String key = entry.getKey();
+            System.out.println("e-mail: " + key + " Name: " + hashMap.get(key));
+        }
+    }
+}
