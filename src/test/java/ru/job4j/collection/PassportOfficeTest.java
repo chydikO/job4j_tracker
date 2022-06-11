491b8bf6f717df6ac8ae60134c5fb507ea77a742
Chudnovskiy.O.W520 <oleg.chudnovskiy@gmail.com>
2022-06-11 21:42:37 +0300

    1. Паспорт и жители [#212656]013

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -1,9 +1,10 @@
 package ru.job4j.collection;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class PassportOfficeTest {
 
@@ -13,5 +14,22 @@ public class PassportOfficeTest {
         PassportOffice office = new PassportOffice();
         office.add(citizen);
         assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+
+    @Test
+    public void addDuplicatePassport() {
+        Citizen citizen1 = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen2 = new Citizen("2f44a", "Ivan Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen1);
+        Assert.assertFalse(office.add(citizen2));
+    }
+
+    @Test
+    public void get() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get("2f44a"), is(citizen));
     }
 }
